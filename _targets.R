7dd7dea4179a23a9e5747edccf51cc5f6da8c647
Hamshaw <shamshaw@usgs.gov>
2021-08-30 14:14:24 -0400

    add a combiner to combine state tally targets and generate data coverage plot

--- _targets.R
+++ _targets.R
@@ -16,30 +16,42 @@ source("1_fetch/src/get_state_inventory.R")
 source("2_process/src/tally_site_obs.R")
 source("3_visualize/src/plot_site_data.R")
 source("3_visualize/src/map_sites.R")
+source("3_visualize/src/plot_data_coverage.R")
 
 # Configuration
-states <- c('WI','MN','MI','IL','IN','IA','VT','NH')
+states <- c('WI','MN','MI','IL','IN','IA','VT')
 parameter <- c('00060')
 
 # Targets
+
+mapped_by_state_targets <- tar_map(
+  values = tibble(state_abb = states) %>%
+    mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png", state_abb)),
+  tar_target(nwis_inventory, get_state_inventory(sites_info = oldest_active_sites, state_abb)),
+  tar_target(nwis_data, get_site_data(nwis_inventory, state_abb, parameter)),
+  tar_target(tally, tally_site_obs(nwis_data)),
+  tar_target(timeseries_png, plot_site_data(out_file = state_plot_files, site_data = nwis_data, parameter)),
+  names = state_abb, unlist=FALSE
+)
+
 list(
   # Identify oldest sites
   tar_target(oldest_active_sites, find_oldest_sites(states, parameter)),
 
-  tar_map(
-    values = tibble(state_abb = states) %>%
-      mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png", state_abb)),
-    tar_target(nwis_inventory, get_state_inventory(sites_info = oldest_active_sites, state_abb)),
-    tar_target(nwis_data, get_site_data(nwis_inventory, state_abb, parameter)),
-    tar_target(tally, tally_site_obs(nwis_data)),
-    tar_target(timeseries_png, plot_site_data(out_file = state_plot_files, site_data = nwis_data, parameter)),
-    names = state_abb
-    ),
+  mapped_by_state_targets,
+
+  tar_combine(obs_tallies, mapped_by_state_targets$tally, command = combine_obs_tallies(!!!.x)),
 
   # Map oldest sites
   tar_target(
     site_map_png,
     map_sites("3_visualize/out/site_map.png", oldest_active_sites),
+    format = "file"
+  ),
+
+  tar_target(
+    data_coverage_png,
+    plot_data_coverage(obs_tallies, "3_visualize/out/data_coverage.png", parameter),
     format = "file"
   )
 )
